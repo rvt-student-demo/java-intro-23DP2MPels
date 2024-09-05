1769cf3c2f1f59ff86462289687b20e9af39a16c
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-09-05 10:23:10 +0000

    "Various Variables" uzdevums par vistam bekonu un traktoru

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,22 +1,26 @@
 package lv.rvt; // klases atrašanas vieta projektā
 
-import java.util.Scanner;
-
 public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Mēs gribam uzzināt no lietotaā: Vārdu Uzvārdu un grupu
-        System.out.println("Uzraksties savu vārdu");
-        String vards = scanner.nextLine();
-        System.out.println("Uzraksties savu vārdu");
-        String uzvards = scanner.nextLine();
-        System.out.println("Uzraksties savu vārdu");
-        String grupa = scanner.nextLine();
 
-        System.out.println(vards+", "+uzvards+", "+grupa);
+        String text = "None!";
+        int chicken = 3;
+        double becon = 5.5;
+
+        System.out.println("Chicken:");
+        System.out.println(chicken);
+        System.out.println("Becon (kg):");
+        System.out.println(becon);
+        System.out.println("Tractor:");
+        System.out.println(text);
+
+        System.out.println("And finally, a summary:");
+        System.out.println(chicken);
+        System.out.println(becon);
+        System.out.println(text);
+
 
     }      
 } 
