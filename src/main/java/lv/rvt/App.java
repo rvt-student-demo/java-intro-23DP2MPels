c6571654b0c71a87973e65e909dce93c1e315da4
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-09-19 08:24:27 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,25 +7,31 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give points [0-100]:");
-        int grade = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet dāvanas vērtību.");
+        int gift = Integer.valueOf(scanner.nextLine());
 
-        if (grade < 0) {
-            System.out.println("Grade: impossible!");
-        } else if (grade <= 49) {
-            System.out.println("Grade: failed");
-        } else if (grade <= 59) {
-            System.out.println("Grade: 1");
-        } else if (grade <= 69) {
-            System.out.println("Grade: 2");
-        } else if (grade <= 79) {
-            System.out.println("Grade: 3");
-        } else if (grade <= 89) {
-            System.out.println("Grade: 4");
-        } else if (grade <= 100) {
-            System.out.println("Grade: 5");
-        } else {
-            System.out.println("Grade: incredible!");
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        } 
+        else if (gift < 25000) {
+            double tax = (100+(gift-5000)*0.08);
+            System.out.println("Tax: "+tax);
+        } 
+        else if (gift < 55000) {
+            double tax = (1700+(gift-25000)*0.1);
+            System.out.println("Tax: "+tax);
+        } 
+        else if (gift < 200000) {
+            double tax = (4700+(gift-55000)*0.12);
+            System.out.println("Tax: "+tax);
+        } 
+        else if (gift < 1000000) {
+            double tax = (22100+(gift-200000)*0.15);
+            System.out.println("Tax: "+tax);
+        }
+        else {
+            double tax = (142100+(gift-1000000)*0.17);
+            System.out.println("Tax: "+tax);
         }
 
     }      
