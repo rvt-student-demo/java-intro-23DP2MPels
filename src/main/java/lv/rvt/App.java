19a6fc299c53c4903cc96f1d8664f6282c096800
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-09-26 08:14:09 +0000

    Average of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,20 +7,19 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         int ciparu_skaits = 0;
-        int summa = 0;
+        int videjais = 0;
 
         while (true) {
             System.out.println("Ierakstiet skaitli.");
             int number = Integer.valueOf(scanner.nextLine());
 
             if (number == 0) {
-                System.out.println("Skaitļu skaits: "+ciparu_skaits);
-                System.out.println("Skaitļu summa: "+summa);
+                System.out.println("Skaitļu vidējais aritmētiskais: "+((double)videjais/(double)ciparu_skaits));
                 break;
             }
             else {
                 ciparu_skaits = ciparu_skaits + 1;
-                summa = summa + number;
+                videjais = videjais + number;
             }
         }
     }
