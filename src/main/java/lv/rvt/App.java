cd70c8cb38de44a9d338e1247835c74eb410d3f0
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-09-26 09:12:01 +0000

    Counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,32 +6,12 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int ciparu_skaits = 0;
-        int videjais = 0;
 
-        while (true) {
-            System.out.println("Ierakstiet skaitli.");
-            int number = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ierakstiet skaitli.");
+        int reiz = Integer.valueOf(scanner.nextLine());
 
-            if (number == 0) {
-                if (ciparu_skaits == 0) {
-                    System.out.println("Nevar izreiķināt vidējo");
-                    break;
-                }
-                else {
-                    System.out.println("Pozitivu skaitļu vidējais aritmētiskais: "+((double)videjais/(double)ciparu_skaits));
-                    break;
-                }
-            }   
-            else {
-                if (number > 0) {
-                    ciparu_skaits = ciparu_skaits + 1;
-                    videjais = videjais + number;
-                }
-                else {
-                    continue;
-                }
-            }
+        for (int i = reiz; i <= 100; i++) {
+            System.out.println(i);
         }
     }
 }
