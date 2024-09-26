325f43642d6a5c7cc971c189d431f938174788b9
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-09-26 10:15:29 +0000

    Repeating, breaking and remembering (5 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,12 +8,35 @@ public class App
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ierakstiet skaitli.");
-        int factorials = Integer.valueOf(scanner.nextLine());
-        int facSumma = 1;
 
-        for (int i = 1; i <= factorials; i++) {
-            facSumma = facSumma * i;
+        int summa = 0;
+        int skaitli = 0;
+        int paraSK = 0;
+        int neparaSK = 0;
+
+        while (true) {
+            int ievads1 = Integer.valueOf(scanner.nextLine());
+        
+            if (ievads1 == -1) {
+                System.out.println("Thx! Bye!");
+                System.out.println("Summa: "+summa);
+                System.out.println("Skaitli: "+skaitli);
+                System.out.println("Vidējais arit. : "+((double)summa/(double)skaitli));
+                System.out.println("Pāra skaitļi: "+paraSK);
+                System.out.println("Nepāra skaitļi: "+neparaSK);
+                break;
+            }
+            else {
+                summa = summa + ievads1;
+                skaitli += 1;
+                if (ievads1%2==0) {
+                    paraSK += 1;
+                }
+                else {
+                    neparaSK += 1;
+                }
+            }
         }
-        System.out.println(facSumma);
+        
     }
 }
