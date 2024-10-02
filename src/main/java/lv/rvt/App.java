31dcb5e42c00805369592465f8d1590583962b7f
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-10-02 10:49:39 +0000

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,40 +3,20 @@ import java.util.*;
 
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ierakstiet skaitli.");
-
-        int summa = 0;
-        int skaitli = 0;
-        int paraSK = 0;
-        int neparaSK = 0;
-
-        while (true) {
-            int ievads1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Sākuma skaitlis... ");
+        int reiz = Integer.valueOf(scanner.nextLine());
+        System.out.println(" ");
+        noVienslidzParametrom(reiz);
         
-            if (ievads1 == -1) {
-                System.out.println("Thx! Bye!");
-                System.out.println("Summa: "+summa);
-                System.out.println("Skaitli: "+skaitli);
-                System.out.println("Vidējais arit. : "+((double)summa/(double)skaitli));
-                System.out.println("Pāra skaitļi: "+paraSK);
-                System.out.println("Nepāra skaitļi: "+neparaSK);
-                break;
-            }
-            else {
-                summa = summa + ievads1;
-                skaitli += 1;
-                if (ievads1%2==0) {
-                    paraSK += 1;
-                }
-                else {
-                    neparaSK += 1;
-                }
-            }
+        };
+
+    public static void noVienslidzParametrom(int parm) {
+        for (int i = 1; i <= parm; i++) {
+            System.out.println(i);
         }
-        
-    }
+    } 
 }
