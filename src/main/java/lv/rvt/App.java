03616ef3a18be1416c05cb9b9d4a7b8f3d95c46a
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-10-03 09:12:28 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,18 +5,20 @@ public class App
 {
     public static void main( String[] args ) {
         
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Sākuma skaitlis... ");
-        int reiz = Integer.valueOf(scanner.nextLine());
-        System.out.println(" ");
-        noVienslidzParametrom(reiz);
+        //System.out.println("Sākuma skaitlis... ");
+        //int reiz = Integer.valueOf(scanner.nextLine());
+        //System.out.println(" ");
+        noApgabalaKasDalasArTris(2,10);
         
         };
 
-    public static void noVienslidzParametrom(int parm) {
-        for (int i = 1; i <= parm; i++) {
-            System.out.println(i);
+    public static void noApgabalaKasDalasArTris(int first,int second) {
+        for (int i = first; i < second + 1; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
         }
     } 
 }
