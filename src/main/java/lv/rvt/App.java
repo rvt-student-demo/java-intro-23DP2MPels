c0371f5f267cb7810835a07a9a8fc178b429b630
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-10-10 08:28:02 +0000

    Advanced astrology (3 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,8 +5,11 @@ public class App
 {
     public static void main(String[] args) {
         Stars.printStars(5);
+        System.out.println("");
         Stars.printStars(3);
+        System.out.println("");
         Stars.printStars(9);
+        System.out.println("");
 
         System.out.println("");
 
@@ -19,5 +22,13 @@ public class App
         System.out.println("");
 
         Stars.printTriangle(4);
+
+        System.out.println("");
+
+        Stars.printTriangleMirror(4);
+
+        System.out.println("");
+
+        Stars.christmasTree(10);
     } 
 }
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -8,12 +8,20 @@ public class Stars {
         for(int i = 1; i <= number; i++) {
             System.out.print("*");
         }
-        System.out.println("");
+        //System.out.println("");
+    }
+
+    public static void printSpace(int number) {
+        for(int i = number; i >= 1; i--) {
+            System.out.print(" ");
+        }
+        //System.out.println("");
     }
 
     public static void printSquare(int size) {
         for(int i = 1; i <= size; i++) {
             Stars.printStars(size);
+            System.out.println("");
         }
 
     }
@@ -21,13 +29,40 @@ public class Stars {
     public static void printRectangle(int width, int height) {
         for(int i = 1; i <= height; i++) {
             Stars.printStars(width);
+            System.out.println("");
         }
     }
     
     public static void printTriangle(int size) {
         for(int i = 1; i <= size; i++) {
             Stars.printStars(i);
+            System.out.println("");
         }
+    }
+
+    public static void printTriangleMirror(int size) {
+        for(int i = 1; i <= size; i++) {
+            Stars.printSpace(size-i);
+            Stars.printStars(i);
+            System.out.println("");
+        }
+    }
+
+    public static void christmasTree(int height) {
+        for(int i = 1; i <= height; i++) {
+            Stars.printSpace(height-i);
+            Stars.printStars(i);
+            Stars.printStars(i-1);
+            System.out.println("");
+        }
+
+        for(int i = 1; i <= 2; i++) {
+            Stars.printSpace(height-2);
+            System.out.print("***");
+            Stars.printSpace(height-2);
+            System.out.println("");
+        }
+           
     }
 }
 
