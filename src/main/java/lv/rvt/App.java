005ec9e28bb88abb3febf4153f4a9b4e9307c34c
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-10-17 09:46:51 +0000

    Exercise 4 — Further Improved Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,39 +8,62 @@ public class App
         Scanner scanner = new Scanner(System.in);
     
         Random rand = new Random();
-        int val = rand.nextInt(1,11);
 
         int i = 0;
-        int score = 5;
+        int won_round = 0;
 
-        System.out.println("I am thinking of a number from 1 to 10. ");
-        System.out.println("Try to guess the number."+"\nYour beginning score is 5 points. "+"\nEach time you guess wrong, your score is decreased by one.");
-        System.out.println("\nEnter a guess:");
+        for(int roundi=1 ; roundi < 11 ; roundi++) {
 
-        for(i=0 ; i < 5 ; i++) {
+            int val = rand.nextInt(1,11);
 
-            int userInput = scanner.nextInt();
+            System.out.println("\nRound "+roundi+":");
 
-            if (userInput == val) {
-                System.out.println("Right!");
+            System.out.println("\nI am thinking of a number from 1 to 10. ");
+            System.out.println("You must guess what it is in three tries.");
+            System.out.println("Enter a guess:");
+
+            for(i=0 ; i < 3 ; i++) {
+
+                int userInput = scanner.nextInt();
+
+                if (userInput == val) {
+                    System.out.println("Right!");
+                    won_round = won_round + 1;
+                    break;
+                }
+                else if (userInput == val+1 || userInput == val-1) {
+                    System.out.println("hot!");
+                }
+                else if (userInput == val+2 || userInput == val-2) {
+                    System.out.println("Warm");
+                }
+                else{
+                    System.out.println("Cold");
+                }
+            }
+
+            if(i >= 3) {
+                System.out.println("The correct number was "+val);
+            }
+
+            System.out.println("You have won "+won_round+" out of "+roundi+" rounds.");
+
+
+        }
+
+        switch (won_round) {
+            case 8:
+                System.out.println("Your rating: amateur.");
                 break;
-            }
-            else if (userInput > val) {
-                score = score - 1;
-                System.out.println("Too high! Your score is "+score);
-            }
-            else{
-                score = score - 1;
-                System.out.println("Too low! Your score is "+score);
-            }
-        }
-
-        if(score > 0) {
-            System.out.println("You have won the game!");
-            System.out.println("Yur score is "+score);
-        }
-        else {
-            System.out.println("You have LOST the game!");
+            case 9:
+                System.out.println("Your rating: professionals.");
+                break;
+            case 10:
+                System.out.println("Your rating: hackers.");
+                break;
+            default:
+                System.out.println("Your rating: amateurs.");
+                break;
         }
 
         
