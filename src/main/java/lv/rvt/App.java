0304f60c2b5546732e73b4f7512622a6fff7f5b4
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-10-17 09:24:09 +0000

    Exercise 3B — Revised Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,32 +11,33 @@ public class App
         int val = rand.nextInt(1,11);
 
         int i = 0;
+        int score = 5;
 
         System.out.println("I am thinking of a number from 1 to 10. ");
-        System.out.println("You must guess what it is in three tries.");
-        System.out.println("Enter a guess:");
+        System.out.println("Try to guess the number."+"\nYour beginning score is 5 points. "+"\nEach time you guess wrong, your score is decreased by one.");
+        System.out.println("\nEnter a guess:");
 
-        for(i=0 ; i < 3 ; i++) {
+        for(i=0 ; i < 5 ; i++) {
 
             int userInput = scanner.nextInt();
 
             if (userInput == val) {
                 System.out.println("Right!");
-                    break;
+                break;
             }
-            else if (userInput == val+1 || userInput == val-1) {
-                System.out.println("hot!");
-            }
-            else if (userInput == val+2 || userInput == val-2) {
-                System.out.println("Warm");
+            else if (userInput > val) {
+                score = score - 1;
+                System.out.println("Too high! Your score is "+score);
             }
             else{
-                System.out.println("Cold");
+                score = score - 1;
+                System.out.println("Too low! Your score is "+score);
             }
-
         }
-        if(i < 3) {
+
+        if(score > 0) {
             System.out.println("You have won the game!");
+            System.out.println("Yur score is "+score);
         }
         else {
             System.out.println("You have LOST the game!");
