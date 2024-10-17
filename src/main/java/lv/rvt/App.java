433d83bc509f66c95fc764c414bb1fa2596aead4
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-10-17 08:31:35 +0000

    Exercise 2 — Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,27 +5,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        ArrayList<Integer> summa = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+    
+        Random rand = new Random();
+        int val = rand.nextInt(1,11);
 
-        int a = 0;
-        int sumbla = 0;
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
+        int i = 0;
 
-            int number = Integer.valueOf(scanner.nextLine());
+        System.out.println("I am thinking of a number from 1 to 10. ");
+        System.out.println("You must guess what it is in three tries.");
+        System.out.println("Enter a guess:");
 
-            if (number == 0) {
+        System.out.println(val);
+
+        for(i=0 ; i < 3 ; i++) {
+
+            int userInput = scanner.nextInt();
+
+            if (val == userInput) {
+                System.out.println("Right!");
                 break;
             }
+            else {
+                System.out.println("WRONG!");
+            }
+        }
+        if(i < 3) {
+            System.out.println("You have won the game!");
+        }
+        else {
+            System.out.println("You have LOST the game!");
+        }
 
-            summa.add(number);
-            sumbla = sumbla + summa.get(a);
-            a=a+1;
-
-        }  
-        System.out.println(summa);
-        System.out.println(summa.get(1)+summa.get(2));
-        System.out.println(sumbla);
+        
 
     } 
 }
