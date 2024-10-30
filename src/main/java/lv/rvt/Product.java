963a156176d0c25865b9ff4e75caaef1157df980
23DP2MPels <166109728+23DP2MPels@users.noreply.github.com>
2024-10-30 11:33:58 +0000

    Products

--- src/main/java/lv/rvt/Product.java
+++ src/main/java/lv/rvt/Product.java
@@ -2,7 +2,17 @@ package lv.rvt;
 
 public class Product {
     private double price;
-    private int quantuty;
+    private int quantity;
     private String name;
     
+    public Product (String initialName, double initialPrice, int initialQuantity) {
+        this.name = initialName;
+        this.price = initialPrice;
+        this.quantity = initialQuantity;
+    }
+    
+    public void printProduct() {
+        System.out.println(this.name + ", price " + this.price + ", " + this.quantity + " pcs");
+    }
+
 }
